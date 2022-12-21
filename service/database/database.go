ea69aca93031b7c0b6d28d77ce11ef47edddfa36
dario-loi <77234595+dario-loi@users.noreply.github.com>
2022-12-21 16:25:28 +0100

    Totally Sane (tm) Non-Generic (R) implementation
    now 100% free from the poisoning constructs of the modern world.

--- service/database/database.go
+++ service/database/database.go
@@ -34,13 +34,24 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"reflect"
 )
 
 // AppDatabase is the high level interface for the DB
 type AppDatabase interface {
 	GetName() (string, error)
 	SetName(name string) error
+	// QueryAndPack executes the query and pakcs the result into the provided interface.
+	// This allows for a *very* generic way of querying the DB and getting back the result.
+	//
+	// This proper use of polymorphism will probably be branded as "bad practice" by the review
+	// and be promptly converted to a hundred different (but equal) methods, with complimentary
+	// `switch` statements, just like the good old days!
+	QueryAndPack(data_struct *interface{}, query string, args ...interface{}) ([]interface{}, error)
 
+	// ExecQuery executes the query and returns an error if the query failed or no rows were affected.
+	// Another generic method to allow for execution of queries that do *not* have a return value.
+	ExecQuery(query string, args ...interface{}) error
 	Ping() error
 }
 
@@ -71,6 +82,71 @@ func New(db *sql.DB) (AppDatabase, error) {
 	}, nil
 }
 
+// Wraps the Ping() method of the underlying DB connection
+// This is used to check if the DB is still alive or to
+// prompt the establishment of a new connection.
 func (db *appdbimpl) Ping() error {
 	return db.c.Ping()
+}
+
+func (db *appdbimpl) ExecQuery(query string, args ...interface{}) error {
+	res, err := db.c.Exec(query, args...)
+
+	if err != nil {
+		return err
+	}
+
+	affected, err := res.RowsAffected()
+
+	if err != nil {
+		return err
+	} else if affected == 0 {
+		return errors.New("no rows affected") // convert to custom error to be properly handled into http response
+	}
+
+	return nil
+}
+
+func (db *appdbimpl) QueryAndPack(data_struct *interface{}, query string, args ...interface{}) ([]interface{}, error) {
+
+	rows, err := db.c.Query(query, args...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() { _ = rows.Close() }()
+
+	cols, err := rows.ColumnTypes()
+
+	if err != nil {
+		return nil, err
+	}
+
+	reflected := reflect.ValueOf(data_struct).Elem()
+
+	for idx, col := range cols {
+		if reflected.FieldByIndex([]int{idx}).Type() != col.ScanType() {
+			return nil, errors.New("type mismatch")
+		}
+	}
+
+	//create slice of structs of the same type as the provided struct
+	//100% safer than generics
+	ret_struct := reflect.MakeSlice(reflect.SliceOf(reflected.Type()), 0, len(cols))
+
+	for rows.Next() {
+		//101% safer than generics
+		row_struct := reflect.New(reflected.Type()).Interface()
+		err = rows.Scan(row_struct)
+		if err != nil {
+			return nil, err
+		}
+
+		ret_struct = reflect.Append(ret_struct, reflect.ValueOf(row_struct).Elem())
+	}
+
+	//102% safer than generics
+	return ret_struct.Interface().([]interface{}), nil
+
 }
