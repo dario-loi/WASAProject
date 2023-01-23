27ab5183005797055e41fad870fbe38a0b27e5da
dario-loi <loi.1940849@studenti.uniroma1.it>
2023-01-23 16:52:18 +0100

    Further Api Refactoring

--- cmd/webapi/migration.sql
+++ cmd/webapi/migration.sql
@@ -1,84 +1,41 @@
-CREATE TABLE IF NOT EXISTS users (
-  id TEXT PRIMARY KEY,
-  name TEXT NOT NULL UNIQUE,
-  propic_id TEXT,
-  bio_id TEXT,
-  FOREIGN KEY(propic_id) REFERENCES photos(id),
-  FOREIGN KEY(bio_id) REFERENCES bios(id)
-);
-CREATE TABLE IF NOT EXISTS followers (
-  followed TEXT,
-  following TEXT,
-  PRIMARY KEY (followed, following),
-  FOREIGN KEY(followed) REFERENCES users(id),
-  FOREIGN KEY(following) REFERENCES users(id)
+CREATE TABLE IF NOT EXISTS Users (
+	ID string,
+	name string
 );
 
-CREATE TABLE IF NOT EXISTS bans (
-  banisher TEXT,
-  banished TEXT,
-  PRIMARY KEY (banisher, banished),
-  FOREIGN KEY(banisher) REFERENCES users(id),
-  FOREIGN KEY(banished) REFERENCES users(id)
+CREATE TABLE IF NOT EXISTS Bans (
+	banisher string,
+	banished string
 );
-CREATE TABLE IF NOT EXISTS bios (
-  id TEXT PRIMARY KEY,
-  bio_str TEXT,
-  birth_date TEXT,
-  residence TEXT,
-  current_place_id TEXT,
-  FOREIGN KEY(residence) REFERENCES places(id),
-  FOREIGN KEY(current_place_id) REFERENCES places(id)
+
+CREATE TABLE IF NOT EXISTS Follower (
+	follower string,
+	followed string
 );
-CREATE TABLE IF NOT EXISTS posts (
-  id TEXT PRIMARY KEY,
-  photo_id TEXT,
-  author TEXT,
-  description TEXT NOT NULL DEFAULT '',
-  creation_date TEXT NOT NULL,
-  FOREIGN KEY(photo_id) REFERENCES photos(id),
-  FOREIGN KEY(id) REFERENCES likes(post_id),
-  FOREIGN KEY(author) REFERENCES users(id)
+
+CREATE TABLE IF NOT EXISTS Posts (
+	post_ID string,
+	photo_code string,
+	poster_ID string,
+	description string,
+	creation_date datetime
 );
-CREATE TABLE IF NOT EXISTS comment (
-  id TEXT PRIMARY KEY,
-  content TEXT NOT NULL DEFAULT '',
-  author_id TEXT,
-  creation_date TEXT NOT NULL,
-  FOREIGN KEY(author_id) REFERENCES users(id)
+
+CREATE TABLE IF NOT EXISTS Likes (
+	post_ID string,
+	liker string
 );
-CREATE TABLE IF NOT EXISTS comment_list (
-  comment_id TEXT,
-  post_id TEXT,
-  FOREIGN KEY(comment_id) REFERENCES comment(id),
-  FOREIGN KEY(post_id) REFERENCES posts(id),
-  PRIMARY KEY (comment_id, post_id)
+
+CREATE TABLE IF NOT EXISTS Comments (
+	post_code string,
+	user_code string,
+	content string,
+	creation_date datetime
 );
-CREATE TABLE IF NOT EXISTS occupations (
-  person_id TEXT,
-  name TEXT NOT NULL,
-  start_date TEXT NOT NULL,
-  end_date TEXT,
-  place_id TEXT,
-  kind TEXT CHECK( kind IN ( 'education', 'employment' ) ) NOT NULL,
-  FOREIGN KEY(person_id) REFERENCES users(id),
-  PRIMARY KEY (person_id, place_id) 
-);
-CREATE TABLE IF NOT EXISTS likes (
-  liker_id TEXT,
-  post_id TEXT, 
-  PRIMARY KEY (liker_id, post_id),
-  FOREIGN KEY(liker_id) REFERENCES users(id)
-);
-CREATE TABLE IF NOT EXISTS photos (
-  id TEXT PRIMARY KEY
-);
-CREATE TABLE IF NOT EXISTS places (
-  id TEXT PRIMARY KEY,
-  country TEXT,
-  state TEXT,
-  city TEXT,
-  lat REAL NOT NULL DEFAULT 0.0,
-  long REAL NOT NULL DEFAULT 0.0,
-  FOREIGN KEY(id) REFERENCES occupations(place_id)
-);
+
+
+
+
+
+
+
